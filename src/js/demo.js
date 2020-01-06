070a60529632d234688b0093a2e29ba19e8704d1
Sam <samuel_morgan_@outlook.com>
2020-01-06 10:31:29 +0000

    created visible remove event listener from dropdown menu selection

--- src/js/demo.js
+++ src/js/demo.js
@@ -3,11 +3,11 @@ import * as elf from './index.js';
 import './index.js';
 
 export const demoSelect = () => {
-    document.getElementById('demoOneBtn').addEventListener('click', function () {
+    document.getElementById('demoOneBtn').addEventListener('click', function() {
         //add demo one class
         makeScreenActive('demoScreenOne');
     });
-    document.getElementById('demoTwoBtn').addEventListener('click', function () {
+    document.getElementById('demoTwoBtn').addEventListener('click', function() {
         //add demo two class
         makeScreenActive('demoScreenTwo');
     });
@@ -22,28 +22,34 @@ export const demoSelect = () => {
 (function initDemoOne() {
     document
         .getElementById('newEventListenerBtn')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             let number = prompt(
                 'Enter a number of button to add event listener to:',
             );
             let el = 'btn' + number;
             let string = prompt('Enter string to print on button press:');
-            let category = prompt('Enter category to place event (leave blank for none):');
-
+            let category = prompt(
+                'Enter category to place event (leave blank for none):',
+            );
 
             if (document.getElementById(el) && string) {
-                elf.createEventListener(el, 'click', function () {
-                    document.getElementById(
-                        'messages',
-                    ).innerHTML += `${string}<br>`;
-                }, category);
+                elf.createEventListener(
+                    el,
+                    'click',
+                    function() {
+                        document.getElementById(
+                            'messages',
+                        ).innerHTML += `${string}<br>`;
+                    },
+                    category,
+                );
             }
             updateEventListenerInfo();
         });
 
     document
         .getElementById('deleteEventListenerBtn')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             let number = prompt(
                 'Enter a number of button to delete event listener from:',
             );
@@ -67,7 +73,7 @@ export const demoSelect = () => {
 
     document
         .getElementById('deleteAllEventListenersBtn')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             let number = prompt(
                 'Enter a number of button to delete all event listeners from:',
             );
@@ -85,7 +91,7 @@ export const demoSelect = () => {
 
     document
         .getElementById('deleteEventListenersByCategoryBtn')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             let category = prompt(
                 'Enter a category of event listeners to delete:',
             );
@@ -94,65 +100,68 @@ export const demoSelect = () => {
         });
     document
         .getElementById('deleteDocumentEventListenersBtn')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             elf.deleteDocumentEventListeners();
             updateEventListenerInfo();
         });
 
     document
         .getElementById('clearMessagesBtn')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             document.getElementById('messages').innerHTML = '';
         });
 
     function updateEventListenerInfo() {
-        document.getElementById('demoOneEventListenerInfo').innerHTML = JSON.stringify(
-            elf.getEventListenerList(),
-        ).replace(/},{/g, '}<br>{');
+        document.getElementById(
+            'demoOneEventListenerInfo',
+        ).innerHTML = JSON.stringify(elf.getEventListenerList()).replace(
+            /},{/g,
+            '}<br>{',
+        );
     }
 })();
 
 (function initDemoTwo() {
     document
         .getElementById('codeNewEventListenerBtn')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             addTextToCode(
                 `createEventListener('elementID','eventType',eventFunction,'eventCategory')`,
             );
         });
 
-    document.getElementById('assignBtnA').addEventListener('click', function () {
+    document.getElementById('assignBtnA').addEventListener('click', function() {
         let string = getInputValue();
         string = string.replace(/elementID|btnA|btnB|btnC/g, 'btnA');
         addTextToCode(string);
     });
-    document.getElementById('assignBtnB').addEventListener('click', function () {
+    document.getElementById('assignBtnB').addEventListener('click', function() {
         let string = getInputValue();
         string = string.replace(/elementID|btnA|btnB|btnC/g, 'btnB');
         addTextToCode(string);
     });
-    document.getElementById('assignBtnC').addEventListener('click', function () {
+    document.getElementById('assignBtnC').addEventListener('click', function() {
         let string = getInputValue();
         string = string.replace(/elementID|btnA|btnB|btnC/g, 'btnC');
         addTextToCode(string);
     });
     document
         .getElementById('assignEventTypeClick')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             let string = getInputValue();
             string = string.replace(/eventType|click|mouseover/g, 'click');
             addTextToCode(string);
         });
     document
         .getElementById('assignEventTypeMouseover')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             let string = getInputValue();
             string = string.replace(/eventType|click|mouseover/g, 'mouseover');
             addTextToCode(string);
         });
     document
         .getElementById('assignAlertEvent')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             let string = getInputValue();
             string = string.replace(
                 /eventFunction|\(\) => alert\([^)]+\)|\(\) => console.log\([^)]+\)/g,
@@ -163,7 +172,7 @@ export const demoSelect = () => {
 
     document
         .getElementById('assignConsoleLogEvent')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             let string = getInputValue();
             string = string.replace(
                 /eventFunction|\(\) => alert\([^)]+\)|\(\) => console.log\([^)]+\)/g,
@@ -173,25 +182,45 @@ export const demoSelect = () => {
         });
     document
         .getElementById('assignCategoryGreeting')
-        .addEventListener('click', function () {
+        .addEventListener('click', function() {
             let string = getInputValue();
-            string = string.replace(/eventCategory|Greeting|Message/g, 'Greeting');
+            string = string.replace(
+                /eventCategory|Greeting|Message/g,
+                'Greeting',
+            );
             addTextToCode(string);
         });
 
-    document.getElementById('assignCategoryMessage')
-        .addEventListener('click', function () {
+    document
+        .getElementById('assignCategoryMessage')
+        .addEventListener('click', function() {
             let string = getInputValue();
-            string = string.replace(/eventCategory|Greeting|Message/g, 'Message');
+            string = string.replace(
+                /eventCategory|Greeting|Message/g,
+                'Message',
+            );
             addTextToCode(string);
         });
 
-    document.getElementById('runCodeBtn').addEventListener('click', function () {
+    document.getElementById('runCodeBtn').addEventListener('click', function() {
         let code = getInputValue();
         let createEventListener = elf.createEventListener;
         eval(code);
         updateEventListenerInfo();
     });
+
+    document
+        .getElementById('removeEventListenerFunctionSelection')
+        .addEventListener('click', function() {
+            showRemoveCodeInstructions(this.value);
+        });
+
+    function showRemoveCodeInstructions(selection) {
+        let elements = document.querySelectorAll('.removeInstructionSelection');
+        elements.forEach(el => el.classList.remove('active'));
+        let selectedElement = document.getElementById(selection);
+        selectedElement.classList.add('active');
+    }
 
     function getTextValue() {
         return document.getElementById('textInput').value;
@@ -206,8 +235,11 @@ export const demoSelect = () => {
     }
 
     function updateEventListenerInfo() {
-        document.getElementById('demoTwoEventListenerInfo').innerHTML = JSON.stringify(
-            elf.getEventListenerList(),
-        ).replace(/},{/g, '}<br>{');
+        document.getElementById(
+            'demoTwoEventListenerInfo',
+        ).innerHTML = JSON.stringify(elf.getEventListenerList()).replace(
+            /},{/g,
+            '}<br>{',
+        );
     }
 })();
